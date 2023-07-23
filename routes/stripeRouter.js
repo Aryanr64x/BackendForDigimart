a784a9e33f5d8dcfb4831b5687f881ab270aea46
saket <aryansaket@yahoo.com>
2023-07-23 17:15:30 +0530

    stripe web hook added

--- routes/stripeRouter.js
+++ routes/stripeRouter.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import { protect } from '../controllers/authController.js';
-import { pay, success } from '../controllers/stripeController.js';
+import { onSessionComplete, pay, success } from '../controllers/stripeController.js';
 const stripeRouter = express.Router();
 
 stripeRouter.post('', protect, pay);
 stripeRouter.post('/success', protect, success);
-
+stripeRouter.post('/hook', onSessionComplete)
 
 
 export default stripeRouter;
