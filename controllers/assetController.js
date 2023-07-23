34839c2c1a13d430576031917e0d728536c0d44d
saket <aryansaket@yahoo.com>
2023-07-23 17:36:11 +0530

    Products can now be created

--- controllers/assetController.js
+++ controllers/assetController.js
@@ -1,6 +1,7 @@
 
 import asyncHandler from 'express-async-handler'
 import { prisma } from "../app.js";
+import { s } from '../app.js'
 
 export const getAssets = asyncHandler(async (req, res) => {
     const assets = await prisma.asset.findMany();
@@ -38,7 +39,7 @@ export const createReview = asyncHandler(async (req, res) => {
     const asset = await prisma.asset.findUnique({
         where: { id: parseInt(req.params.id) },
     })
-   
+
 
     const newRating = computeNewAverage(asset.average_rating, asset.reviews_count, req.body.rating)
 
@@ -51,7 +52,7 @@ export const createReview = asyncHandler(async (req, res) => {
     await prisma.review.create({ data: { body: req.body.text, asset_id: parseInt(req.params.id), user_id: req.body.user.id } });
 
 
-    const newAsset =  await prisma.asset.findUnique({
+    const newAsset = await prisma.asset.findUnique({
         where: {
             id: parseInt(req.params.id)
         },
@@ -68,3 +69,36 @@ export const createReview = asyncHandler(async (req, res) => {
         data: newAsset
     })
 })
+
+
+
+
+export const createAsset = (asyncHandler(async (req, res) => {
+
+    const product = await s.products.create({
+        name: req.body.title,
+        description: req.body.summary,
+        type: 'service', // You can use 'good' for physical products
+    });
+
+
+    const price = await s.prices.create({
+        unit_amount: req.body.price * 10, // Amount in cents (e.g., $10.00)
+        currency: 'usd',
+        product: product.id, // Use the product ID from the created product
+
+    });
+
+    const asset = await prisma.asset.create({
+        data: {
+            description: req.body.description,
+            price: req.body.price,
+            summary: req.body.summary,
+            title: req.body.title,
+            dp: req.body.dp,
+            creator_id: req.body.user.id,
+            priceId: price.id
+        }
+    })
+    return asset
+}))
--- routes/assetRouter.js
+++ routes/assetRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {createReview, getAsset, getAssets} from "../controllers/assetController.js";
+import { createAsset, createReview, getAsset, getAssets } from "../controllers/assetController.js";
 import { protect } from "../controllers/authController.js";
 const assetRouter = express.Router();
 
 assetRouter.get("", getAssets);
+assetRouter.post("", protect, createAsset)
 assetRouter.get("/:id", getAsset);
-assetRouter.post('/:id/review', protect ,createReview)
+assetRouter.post('/:id/review', protect, createReview)
 
-export  default  assetRouter;
+export default assetRouter;
